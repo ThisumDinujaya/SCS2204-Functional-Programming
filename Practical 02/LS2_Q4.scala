804048ebc1fd15ba8c3262eb35a0ed99fd325c16
Thisum Dinujaya <140166006+ThisumDinujaya@users.noreply.github.com>
2024-06-25 09:24:15 +0530

    LS2_Q4

--- Practical 02/LS2_Q4.scala
+++ Practical 02/LS2_Q4.scala
@@ -1,3 +1,17 @@
 object LS2_Q4 {
-    
+  def main(args: Array[String]) = {
+    println("If ticket price = Rs. 5, profit = Rs. " + profit(5));
+    println("If ticket price = Rs. 10, profit = Rs. " + profit(10));
+    println("If ticket price = Rs. 15, profit = Rs. " + profit(15));
+    println("If ticket price = Rs. 20, profit = Rs. " + profit(20));
+    println("If ticket price = Rs. 25, profit = Rs. " + profit(25));
+    println("If ticket price = Rs. 30, profit = Rs. " + profit(30));
+    println("If ticket price = Rs. 30, profit = Rs. " + profit(35));
+    println("If ticket price = Rs. 30, profit = Rs. " + profit(40));
+
+    def attendees(price: Int): Int = 120 + (15 - price) / 5 * 20
+    def revenue(price: Int): Int = attendees(price) * price
+    def cost(price: Int): Int = 500 + attendees(price) * 3
+    def profit(price: Int): Int = revenue(price) - cost(price)
+  }
 }
