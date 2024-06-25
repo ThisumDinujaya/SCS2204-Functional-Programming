3ceb87771d08732daa7ffe5602c561c9360bc7d1
Thisum Dinujaya <140166006+ThisumDinujaya@users.noreply.github.com>
2024-06-25 06:00:42 +0530

    LS2

---
+++ Practical 02/LS2_Q2.scala
@@ -0,0 +1,24 @@
+object LS2_Q2 {
+    def main(args: Array[String]) = {
+      var a: Int = 2;
+      var b: Int = 3;
+      var c: Int = 4;
+      var d: Int = 5;
+
+      var k: Float = 4.3f;
+
+      b -= 1;
+      println(b * a + c *d);
+      d -= 1;
+      println (a);
+      a += 1;
+      println (-2 * ( g - k ) + c);
+      var x: Int = c;
+      println (x == c);
+      c += 1;
+      x = c;
+      c += 1;
+      println (x == c * a);
+      a += 1;
+   }
+}
