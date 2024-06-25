7c7244853f37af0c858e73ab75b61fe69cc80a3a
Thisum Dinujaya <140166006+ThisumDinujaya@users.noreply.github.com>
2024-06-25 06:33:12 +0530

    LS2

--- Practical 02/LS2_Q3.scala
+++ Practical 02/LS2_Q3.scala
@@ -1,3 +1,13 @@
 object LS2_Q3 {
-    
+  def main(args: Array[String]) = {
+    println("Take home salary of an employee = Rs. " + takeHome(40, 30));
+
+    def normal(hours: Int): Int = hours * 250;
+    def ot(hours: Int): Int = hours * 85;
+    def income(norH: Int, otH: Int): Int = normal(norH) + ot(otH);
+    def tax(income: Int): Double = income * 0.12;
+    def takeHome(norH: Int, otH: Int): Double = income(norH, otH) - tax(income(norH, otH))
+
+  }
 }
+ 
