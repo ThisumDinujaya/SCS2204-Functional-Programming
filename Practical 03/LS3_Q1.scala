8b0a440c5c3aff00720bcc1a97f7fa1fd13dd728
Thisum Dinujaya <140166006+ThisumDinujaya@users.noreply.github.com>
2024-07-02 02:15:03 +0530

    Create L3_Q1.scala

---
+++ Practical 03/LS3_Q1.scala
@@ -0,0 +1,10 @@
+import scala.io.StdIn.readLine;
+
+
+object LS3_Q1 {
+  def main(args: Array[String]) = {
+    println("Input a string : ");
+    var input = readLine();
+    println("Reversed string : " + input.reverse);
+  }
+}
